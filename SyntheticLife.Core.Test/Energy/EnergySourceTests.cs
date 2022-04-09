09dfb444309ccb457a45d05c597fb39d262ea0be
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-10 00:54:15 +1000

    Change Energy to double from int.

--- SyntheticLife.Core.Test/Energy/EnergySourceTests.cs
+++ SyntheticLife.Core.Test/Energy/EnergySourceTests.cs
@@ -18,21 +18,21 @@ public class EnergySourceTests
     public void EnergySetOnConstruction()
     {
         // Arrange
-        var energySource = new EnergySource(Location, 100);
+        var energySource = new EnergySource(Location, 100.2);
         // Act
         var energy = energySource.Energy;
         // Assert
-        Assert.That(energy, Is.EqualTo(100));
+        Assert.That(energy, Is.EqualTo(100.2));
     }
 
     [Test]
     public void EnergySet()
     {
         // Arrange
-        var energySource = new EnergySource(Location, 1310);
+        var energySource = new EnergySource(Location, 1310.4);
         // Act
         energySource.Energy -= 100;
         // Assert
-        Assert.That(energySource.Energy, Is.EqualTo(1210));
+        Assert.That(energySource.Energy, Is.EqualTo(1210.4));
     }
 }
