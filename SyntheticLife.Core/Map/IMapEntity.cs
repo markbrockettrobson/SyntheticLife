0e3617515e0571f5f11da64de1c76fda3dffec53
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-09 22:44:34 +1000

    Add IMapEntity and MapEntityBase for a base for location queryable objects.

---
+++ SyntheticLife.Core/Map/IMapEntity.cs
@@ -0,0 +1,10 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace SyntheticLife.Core.Map
+{
+    public interface IMapEntity
+    {
+        public Envelope Location { get; set; }
+        public void AddOnUpdateLocationAction(Action<IMapEntity, Envelope> updateAction);
+    }
+}
---
+++ SyntheticLife.Core/Map/MapEntityBase.cs
@@ -0,0 +1,35 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace SyntheticLife.Core.Map
+{
+    public class MapEntityBase : IMapEntity
+    {
+        private Envelope location = new Envelope();
+        public Envelope Location
+        {
+            get => location;
+            set
+            {
+                foreach (var updateAction in UpdateActions)
+                {
+                    updateAction(this, value);
+                }
+
+                location = value;
+            }
+        }
+
+        private IList<Action<IMapEntity, Envelope>> UpdateActions { get; }
+
+        public MapEntityBase(Envelope location)
+        {
+            UpdateActions = new List<Action<IMapEntity, Envelope>>();
+            Location = location;
+        }
+
+        public void AddOnUpdateLocationAction(Action<IMapEntity, Envelope> updateAction)
+        {
+            UpdateActions.Add(updateAction);
+        }
+    }
+}
