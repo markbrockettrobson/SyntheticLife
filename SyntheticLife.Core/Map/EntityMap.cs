1b73564440aac6dc8ef65a181e7a0477a12174ea
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-09 22:52:03 +1000

    Add data structure to queary map entries.

---
+++ SyntheticLife.Core/Map/EntityMap.cs
@@ -0,0 +1,51 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.Index.Quadtree;
+
+namespace SyntheticLife.Core.Map
+{
+    public class EntityMap : IEntityMap
+    {
+        public IEnumerable<IMapEntity> MapEntities => Tree.QueryAll();
+        private Quadtree<IMapEntity> Tree { get; set; }
+
+        public EntityMap()
+        {
+            Tree = new Quadtree<IMapEntity>();
+        }
+
+        public void AddEntity(IMapEntity entity)
+        {
+            AddEntity(entity, entity.Location);
+            entity.AddOnUpdateLocationAction(OnEntityLocationUpdate);
+        }
+
+        private void AddEntity(IMapEntity entity, Envelope location)
+        {
+            if (MapEntities.Contains(entity))
+            {
+                throw new InvalidOperationException(string.Format("Entiry {0} already in Map.", entity));
+            }
+
+            Tree.Insert(location, entity);
+        }
+
+        public void RemoveEntity(IMapEntity entity)
+        {
+            if (!Tree.Remove(entity.Location, entity))
+            {
+                throw new InvalidOperationException(string.Format("Entiry {0} was not located in Map.", entity));
+            }
+        }
+
+        public IList<IMapEntity> Query(Envelope envelope)
+        {
+            return Tree.Query(envelope);
+        }
+
+        private void OnEntityLocationUpdate(IMapEntity entity, Envelope newLocation)
+        {
+            RemoveEntity(entity);
+            AddEntity(entity, newLocation);
+        }
+    }
+}
---
+++ SyntheticLife.Core/Map/IEntityMap.cs
@@ -0,0 +1,13 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace SyntheticLife.Core.Map
+{
+    public interface IEntityMap
+    {
+        public IEnumerable<IMapEntity> MapEntities { get; }
+
+        public void AddEntity(IMapEntity entity);
+        public void RemoveEntity(IMapEntity entity);
+        public IList<IMapEntity> Query(Envelope envelope);
+    }
+}
