3904c0219014a21d24b05ff2b88e9103fbd31d22
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-10 02:19:04 +1000

    Fix test namespace.

--- SyntheticLife.Core.Test/Energy/EnergySourceTests.cs
+++ SyntheticLife.Core.Test/Energy/EnergySourceTests.cs
@@ -2,37 +2,39 @@ using NetTopologySuite.Geometries;
 using NUnit.Framework;
 using SyntheticLife.Core.Energy;
 
-namespace SyntheticLife.Core.Test;
-
-public class EnergySourceTests
+namespace SyntheticLife.Core.Test
 {
-    public Envelope Location = new ();
-
-    [SetUp]
-    public void SetUp()
+    [TestFixture]
+    public class EnergySourceTests
     {
-        Location = new Envelope(1, 1, 1, 1);
-    }
+        public Envelope Location = new ();
 
-    [Test]
-    public void EnergySetOnConstruction()
-    {
-        // Arrange
-        var energySource = new EnergySource(Location, 100.2);
-        // Act
-        var energy = energySource.Energy;
-        // Assert
-        Assert.That(energy, Is.EqualTo(100.2));
-    }
+        [SetUp]
+        public void SetUp()
+        {
+            Location = new Envelope(1, 1, 1, 1);
+        }
 
-    [Test]
-    public void EnergySet()
-    {
-        // Arrange
-        var energySource = new EnergySource(Location, 1310.4);
-        // Act
-        energySource.Energy -= 100;
-        // Assert
-        Assert.That(energySource.Energy, Is.EqualTo(1210.4));
+        [Test]
+        public void EnergySetOnConstruction()
+        {
+            // Arrange
+            var energySource = new EnergySource(Location, 100.2);
+            // Act
+            var energy = energySource.Energy;
+            // Assert
+            Assert.That(energy, Is.EqualTo(100.2));
+        }
+
+        [Test]
+        public void EnergySet()
+        {
+            // Arrange
+            var energySource = new EnergySource(Location, 1310.4);
+            // Act
+            energySource.Energy -= 100;
+            // Assert
+            Assert.That(energySource.Energy, Is.EqualTo(1210.4));
+        }
     }
 }
