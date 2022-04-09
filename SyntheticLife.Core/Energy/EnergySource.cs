e4ddebfeba4006c39986f2d4b3b6298904feae33
Mark Brockett Robson <47262668+markbrockettrobson@users.noreply.github.com>
2022-04-10 00:24:56 +1000

    Add EnergySource Class (#4)

---
+++ SyntheticLife.Core/Energy/EnergySource.cs
@@ -0,0 +1,14 @@
+﻿using NetTopologySuite.Geometries;
+using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.Energy
+{
+    public class EnergySource : MapEntityBase, IEnergySource
+    {
+        public int Energy { get; set; }
+        public EnergySource(Envelope location, int energy) : base(location)
+        {
+            Energy = energy;
+        }
+    }
+}
