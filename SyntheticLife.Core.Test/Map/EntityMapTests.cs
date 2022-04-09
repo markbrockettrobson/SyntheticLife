6d4c5e6c7ab2c4515080068b1e1cd08403a7d390
Mark Brockett Robson <47262668+markbrockettrobson@users.noreply.github.com>
2022-04-09 23:10:22 +1000

    Map (#3)
    * Remove sample test file.
    * Add NetTopologySuite to SyntheticLife.Core
    * Add Moq to SyntheticLife.Core.Test.
    * Add IMapEntity and MapEntityBase for a base for location queryable objects.
    * Add data structure to queary map entries.

---
+++ SyntheticLife.Core.Test/Map/EntityMapTests.cs
@@ -0,0 +1,205 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NetTopologySuite.Geometries;
+using NUnit.Framework;
+using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.Test;
+
+public class EntityMapTests
+{
+    public List<Mock<IMapEntity>> TestEntities { get; set; } = new ();
+    public Action<IMapEntity, Envelope>? LastOnUpdateLocationAction { get; set; } = null;
+
+    [SetUp]
+    public void SetUp()
+    {
+        for (int index = 0; index < 10; index++)
+        {
+            Mock<IMapEntity> entity = new ();
+            entity
+                .Setup(entity => entity.Location)
+                .Returns(new Envelope(index, index + 0.5, index, index + 0.5));
+            entity
+                .Setup(entity => entity.AddOnUpdateLocationAction(It.IsAny<Action<IMapEntity, Envelope>>()))
+                .Callback<Action<IMapEntity, Envelope>>(action => LastOnUpdateLocationAction = action);
+
+            TestEntities.Add(entity);
+        }
+    }
+
+    [Test]
+    public void MapEntitiesEmpty()
+    {
+        // Arrange
+        var map = new EntityMap();
+        // Act
+        var mapEntities = map.MapEntities;
+        // Assert
+        Assert.That(mapEntities, Is.Empty);
+    }
+
+    [TestCase(new[] { 0 })]
+    [TestCase(new[] { 0, 1 })]
+    [TestCase(new[] { 0, 2, 3 })]
+    [TestCase(new[] { 0, 2, 4, 6, 8 })]
+    public void MapEntities(int[] entityIndexes)
+    {
+        // Arrange
+        var map = new EntityMap();
+        foreach (var index in entityIndexes)
+        {
+            map.AddEntity(TestEntities[index].Object);
+        }
+
+        // Act
+        var mapEntities = map.MapEntities;
+        // Assert
+        Assert.That(mapEntities, Is.EquivalentTo(entityIndexes.Select(index => TestEntities[index].Object)));
+    }
+
+    [Test]
+    public void AddExceptionOnRepeat()
+    {
+        // Arrange
+        var map = new EntityMap();
+        map.AddEntity(TestEntities[0].Object);
+        // Act
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => map.AddEntity(TestEntities[0].Object));
+    }
+
+    [Test]
+    public void AddEntityOnEntityLocationUpdateSet()
+    {
+        // Arrange
+        var map = new EntityMap();
+        map.AddEntity(TestEntities[0].Object);
+        // Act
+        // Assert
+        TestEntities[0].Verify(
+            entity => entity.AddOnUpdateLocationAction(It.IsAny<Action<IMapEntity, Envelope>>()),
+            Times.Once);
+    }
+
+    [Test]
+    public void QueryEmpty()
+    {
+        // Arrange
+        var map = new EntityMap();
+        // Act
+        var mapEntities = map.Query(new Envelope(-10, 10, -10, 10));
+        // Assert
+        Assert.That(mapEntities, Is.Empty);
+    }
+
+    [TestCase(new[] { 0 })]
+    [TestCase(new[] { 0, 1 })]
+    [TestCase(new[] { 0, 2, 3 })]
+    [TestCase(new[] { 0, 2, 4, 6, 8 })]
+    public void QueryAllInside(int[] entityIndexes)
+    {
+        // Arrange
+        var map = new EntityMap();
+        foreach (var index in entityIndexes)
+        {
+            map.AddEntity(TestEntities[index].Object);
+        }
+
+        // Act
+        var mapEntities = map.Query(new Envelope(-10, 10, -10, 10));
+        // Assert
+        Assert.That(mapEntities, Is.EquivalentTo(entityIndexes.Select(index => TestEntities[index].Object)));
+    }
+
+    [TestCase(new[] { 0 })]
+    [TestCase(new[] { 0, 1 })]
+    [TestCase(new[] { 0, 2, 3 })]
+    [TestCase(new[] { 0, 2, 4, 6, 8 })]
+    public void QueryAllOutside(int[] entityIndexes)
+    {
+        // Arrange
+        var map = new EntityMap();
+        foreach (var index in entityIndexes)
+        {
+            map.AddEntity(TestEntities[index].Object);
+        }
+
+        // Act
+        var mapEntities = map.Query(new Envelope(-10, -0.1, -10, -0.1));
+        // Assert
+        Assert.That(mapEntities, Is.Empty);
+    }
+
+    [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 7 }, new[] { 4, 5, 6, 7 })]
+    [TestCase(new[] { 0, 2, 3, 6, 8 }, new[] { 6, 8 })]
+    public void QueryMixed(int[] entityIndexes, int[] entityIndexesInside)
+    {
+        // Arrange
+        var map = new EntityMap();
+        foreach (var index in entityIndexes)
+        {
+            map.AddEntity(TestEntities[index].Object);
+        }
+
+        // Act
+        var mapEntities = map.Query(new Envelope(4, 10, 4, 10));
+        // Assert
+        Assert.That(mapEntities, Is.EquivalentTo(entityIndexesInside.Select(index => TestEntities[index].Object)));
+    }
+
+    [TestCase(new[] { 0, 1, 2, 3 }, new[] { 0, 2, 3 })]
+    [TestCase(new[] { 0, 2, 3, 6, 8 }, new[] { 6, 2, 3 })]
+    public void Delete(int[] entityIndexes, int[] insideEntityIndexes)
+    {
+        // Arrange
+        var map = new EntityMap();
+        foreach (var index in entityIndexes)
+        {
+            map.AddEntity(TestEntities[index].Object);
+        }
+
+        // Act
+        foreach (var index in entityIndexes.Where(index => !insideEntityIndexes.Contains(index)))
+        {
+            map.RemoveEntity(TestEntities[index].Object);
+        }
+
+        // Assert
+        var mapEntities = map.Query(new Envelope(0, 10, 0, 10));
+        Assert.That(mapEntities, Is.EquivalentTo(insideEntityIndexes.Select(index => TestEntities[index].Object)));
+    }
+
+    [Test]
+    public void DeleteExceptionOnRepeat()
+    {
+        // Arrange
+        var map = new EntityMap();
+        // Act
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => map.RemoveEntity(TestEntities[0].Object));
+    }
+
+    [Test]
+    public void EntityLocationUpdate()
+    {
+        // Arrange
+        var map = new EntityMap();
+        map.AddEntity(TestEntities[0].Object);
+        // Act
+        if (LastOnUpdateLocationAction == null)
+        {
+            throw new Exception("LastOnUpdateLocationAction should be set.");
+        }
+
+        LastOnUpdateLocationAction(TestEntities[0].Object, new Envelope(30, 31, 30, 31));
+        // Assert
+        var entitiesAtOldLocation = map.Query(new Envelope(0, 10, 0, 10));
+        var entitiesAtNewLocation = map.Query(new Envelope(30, 31, 30, 31));
+
+        Assert.That(entitiesAtOldLocation, Has.Count.EqualTo(0));
+        Assert.That(entitiesAtNewLocation, Is.EquivalentTo(new List<IMapEntity>() { TestEntities[0].Object }));
+    }
+}
