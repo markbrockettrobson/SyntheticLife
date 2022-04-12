28862638357f5c2b2aefaaaa5d8ee6baa2336d92
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-12 22:09:29 +1000

    add MoveToClosestEnergySourceBehaviourEngine with tests.

---
+++ SyntheticLife.Core/LifeForm/MoveToClosestEnergySourceBehaviourEngine.cs
@@ -0,0 +1,39 @@
+﻿using SyntheticLife.Core.Energy;
+using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.LifeForm
+{
+    public class MoveToClosestEnergySourceBehaviourEngine : IBehaviourEngine
+    {
+        private Random RandomGenirator { get; }
+        private double SearchAreaSize { get; set; }
+
+        public MoveToClosestEnergySourceBehaviourEngine(Random random, double searchAreaSize)
+        {
+            RandomGenirator = random;
+            SearchAreaSize = searchAreaSize;
+        }
+
+        public IMovementOrder SelectMovement(ICreature creature, IEntityMap entityMap)
+        {
+            var searchArea = creature.Location.Copy();
+            searchArea.ExpandBy(SearchAreaSize);
+
+            var closestEntity = entityMap
+                .Query(searchArea)
+                .Where(entity => typeof(IEnergySource).IsInstanceOfType(entity))
+                .MinBy(entity => entity.Location.Centre.Distance(creature.Location.Centre));
+
+            if (closestEntity == null)
+            {
+                return new MovementOrder(
+                    creature.Location,
+                    creature.Location.TranslateRandomDirection(RandomGenirator, creature.Species.MovementSpeed));
+            }
+
+            return new MovementOrder(
+                creature.Location,
+                creature.Location.TranslateTo(closestEntity.Location, creature.Species.MovementSpeed));
+        }
+    }
+}
