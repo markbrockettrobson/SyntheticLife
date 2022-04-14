c49eaebb153fb140cba7fa5988abdda932381869
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-14 23:06:28 +1000

    Add IConsumptionEngine.

---
+++ SyntheticLife.Core/Interface/LifeForm/IConsumptionEngine.cs
@@ -0,0 +1,10 @@
+﻿using SyntheticLife.Core.Energy;
+using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.LifeForm
+{
+    public interface IConsumptionEngine
+    {
+        public IEnumerable<IEnergyConsumptionOrder> ProssesCurrectPosition(ICreature creature, IEntityMap entityMap);
+    }
+}
