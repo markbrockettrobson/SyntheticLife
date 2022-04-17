98e6033f0e26c50ae78a7ac6e59c3f13bec474b9
markbrockettrobson <markbrockettrobson@gmail.com>
2022-04-17 18:21:41 +1000

    Add RandomEnergyDistributor.

---
+++ SyntheticLife.Core/Interface/Energy/IEnergyDistributor.cs
@@ -0,0 +1,9 @@
+﻿using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.Energy
+{
+    public interface IEnergyDistributor
+    {
+        public IEnumerable<IEnergySource> CreateEnergySources(IEntityMap map, double energyAmount);
+    }
+}
---
+++ SyntheticLife.Core/src/Energy/EnergyDistributor.cs
@@ -0,0 +1,42 @@
+﻿using Ardalis.GuardClauses;
+using SyntheticLife.Core.Map;
+
+namespace SyntheticLife.Core.Energy
+{
+    public class RandomEnergyDistributor : IEnergyDistributor
+    {
+        private Random RandomNumberGenerator { get; }
+        private double MaxSize { get; }
+
+        public RandomEnergyDistributor(Random random, double maxSize)
+        {
+            Guard.Against.Negative(maxSize);
+
+            RandomNumberGenerator = random;
+            MaxSize = maxSize;
+        }
+
+        public IEnumerable<IEnergySource> CreateEnergySources(IEntityMap map, double energyAmount)
+        {
+            Guard.Against.Negative(energyAmount);
+            var energyList = new List<IEnergySource>();
+
+            while (energyAmount > 0)
+            {
+                var location = map.Bounds.RandomEnvelopeInside(RandomNumberGenerator, 1);
+                if (energyAmount >= MaxSize)
+                {
+                    energyList.Add(new EnergySource(location, MaxSize));
+                    energyAmount -= MaxSize;
+                }
+                else
+                {
+                    energyList.Add(new EnergySource(location, energyAmount));
+                    energyAmount = 0;
+                }
+            }
+
+            return energyList;
+        }
+    }
+}
